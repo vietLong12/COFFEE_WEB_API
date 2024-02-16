f1648349d9f62512c34d938146cc27146394bd07
vietLong12 <vietlongn358@gmail.com>
2024-02-16 17:24:11 +0700

    xu li phan cmt

--- src/index.js
+++ src/index.js
@@ -70,21 +70,69 @@ app.use(morgan("short"));
 route(app);
 
 let index = 0;
+let listComment = []
 io.on('connection', (socket) => {
-  index++;
-  console.log(
-    'A new user connected: ' + socket.id + ', total actives: ' + index
-  );
+
   socket.on('disconnect', () => {
     index--;
     console.log('A user disconnected: ' + socket.id + ', total actives: ' + index);
   });
+  socket.on("adminLive", () => {
+    const temp = [...listComment]
+    const slicedCommentList = temp.slice(Math.max(listComment.length - 100, 0));
+    socket.emit("adminComment", slicedCommentList);
+  })
+
+  socket.on("sendComment", (data) => {
+    listComment.push(data);
+    const temp = [...listComment]
+    const slicedCommentList = temp.slice(Math.max(listComment.length - 100, 0));
+    io.emit("listComment", slicedCommentList);
+    io.emit("adminComment", slicedCommentList);
+  })
+
+  socket.on("stream", (data) => {
+    console.log('data: ', data);
+    io.emit("live", data)
+  })
 
   socket.on("joinLive", (data) => {
-    console.log("Co nguoi moi dang nhap")
-    socket.emit("viewCount", index);
+    index++;
+    console.log(
+      'Có người mới tham gia live: ' + socket.id + ', Tên người tham gia: ' + data
+    );
+    console.log(`Hiện có ${index} viewer`)
+    io.emit("viewCount", index);
+    io.emit("listComment", listComment);
+  })
+  socket.on("viewerOutLive", () => {
+    index--;
+    io.emit("viewCount", index - 1);
   })
 });
+
+
+
+const os = require('os');
+
+// Lấy địa chỉ IP của máy tính
+function getIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const interfaceName in interfaces) {
+    const interface = interfaces[interfaceName];
+    for (const { address, family, internal } of interface) {
+      if (family === 'IPv4' && !internal) {
+        return address;
+      }
+    }
+  }
+  return 'Không thể lấy địa chỉ IP';
+}
+
+// Sử dụng hàm để lấy địa chỉ IP và in ra
+
 server.listen(port, () => {
+  const ipAddress = getIPAddress();
+  console.log('Địa chỉ IP của máy tính:', ipAddress);
   console.log(`App listening on port http://localhost:${port}`);
 });
